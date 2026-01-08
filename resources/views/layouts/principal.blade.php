9cc7b89a12f3456e75bbd788cfbbaedb9ebce686
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2026-01-08 11:05:14 -0600

    Agregar productos a página principal

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -14,7 +14,7 @@
 
 
     <div class="">
-        @livewire('product-card-list', ['title' => 'PRODUCTOS', 'destacadoValue' => 0, 'limit' => 3])
+        @livewire('product-card-list', ['title' => 'PRODUCTOS', 'destacadoValue' => 0, 'limit' => 6])
     </div>
 
     @include('layouts.principal-footer')
