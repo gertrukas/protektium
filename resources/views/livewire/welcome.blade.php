be7a5d3861d7fa6e19f206ff7fdc217b057f8fbd
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2026-01-27 21:00:47 -0600

    Página principal se quita el duplicado de Hero Section

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,7 +1,7 @@
 <section class="container">
     <!-- Productos -->
 
-    @include('components.home-hero-section')
+    {{-- @include('components.home-hero-section') --}}
 
     <div class="">
         @livewire('blog-card-list', ['title' => 'COMUNICADOS', 'limit' => env('LIMIT_SHOW_BLOGS_INITIAL_PAGE', 3)])
