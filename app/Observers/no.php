33e498f9b7c2aedac7f08fb07627ffc1f3c421f4
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2026-01-29 10:22:18 -0600

    Actualización de paquetes con composer update

---
+++ app/Observers/no.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\xx;
+
+class no
+{
+    /**
+     * Handle the xx "created" event.
+     */
+    public function created(xx $xx): void
+    {
+        //
+    }
+
+    /**
+     * Handle the xx "updated" event.
+     */
+    public function updated(xx $xx): void
+    {
+        //
+    }
+
+    /**
+     * Handle the xx "deleted" event.
+     */
+    public function deleted(xx $xx): void
+    {
+        //
+    }
+
+    /**
+     * Handle the xx "restored" event.
+     */
+    public function restored(xx $xx): void
+    {
+        //
+    }
+
+    /**
+     * Handle the xx "force deleted" event.
+     */
+    public function forceDeleted(xx $xx): void
+    {
+        //
+    }
+}
